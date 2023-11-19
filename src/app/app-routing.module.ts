5942554aa8d31e1a100da7ee20e27b0072b2612a
Bruno Silva <brunovinicius0704@gmail.com>
2023-11-19 19:10:37 -0300

    remove homeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { 
     path: '',
     component: LayoutComponent,
     children: [
-      { path: '', component: HomeComponent },
       { 
-        path: 'exemples', 
+        path: '', 
         children: [
           { path: '', loadChildren: () => import('./pages/exemples/exemples.module').then(m => m.ExemplesModule) }
         ] 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { LayoutModule } from './layout/layout.module';
 import { ExemplesModule } from './pages/exemples/exemples.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
